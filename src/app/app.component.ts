85fe866f0d3e8d63da1b0e2e95003b1db8259056
kamal shekho <k.shekho@yahoo.com>
2025-07-12 02:24:19 +0200

    update

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,6 +1,7 @@
-import { Component, HostListener, signal } from '@angular/core';
+import { Component, HostListener, OnInit, signal } from '@angular/core';
 import { SidebarComponent } from './components/sidebar/sidebar.component';
 import { MainComponent } from './main/main.component';
+import { SMALL_SCREEN_WIDTH } from './constants/screen-sizes';
 
 @Component({
   selector: 'app-root',
@@ -8,16 +9,20 @@ import { MainComponent } from './main/main.component';
   templateUrl: './app.component.html',
   styleUrl: './app.component.css',
 })
-export class AppComponent {
+export class AppComponent implements OnInit {
   isLeftSidebarCollapsed = signal<boolean>(false);
   screenWidth = signal<number>(window.innerWidth);
 
   @HostListener('window:resize')
   onResize() {
     this.screenWidth.set(window.innerWidth);
-    if (this.screenWidth() < 768) {
+    if (this.screenWidth() < SMALL_SCREEN_WIDTH) {
       this.isLeftSidebarCollapsed.set(true);
     }
+  }
+
+  ngOnInit(): void {
+    this.isLeftSidebarCollapsed.set(this.screenWidth() < SMALL_SCREEN_WIDTH);
   }
 
   public changeIsLeftSidebarCollapsed(isLeftSidebarCollapsed: boolean): void {
