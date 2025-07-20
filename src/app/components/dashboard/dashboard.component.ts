7ad9f18f9fa0179a144beb2d1bb6904ce27da28d
kamal shekho <k.shekho@yahoo.com>
2025-07-20 14:19:12 +0200

    adjust number cards dashboard

--- src/app/components/dashboard/dashboard.component.ts
+++ src/app/components/dashboard/dashboard.component.ts
@@ -1,4 +1,8 @@
 import { Component } from '@angular/core';
+import { Electrician } from '../../core/model/electricians.model';
+import { ElectriciansService } from '../../core/services/electricians.service';
+import { RequestsService } from '../../core/services/requests.service';
+import { Request } from '../../core/model/request.model';
 
 @Component({
   selector: 'app-dashboard',
@@ -6,4 +10,32 @@ import { Component } from '@angular/core';
   templateUrl: './dashboard.component.html',
   styleUrl: './dashboard.component.css',
 })
-export class DashboardComponent {}
+export class DashboardComponent {
+  electricians: Electrician[] = [];
+  openRequests: Request[] = [];
+  availableElectricians: Electrician[] = [];
+
+  constructor(
+    private requestsService: RequestsService,
+    private electriciansService: ElectriciansService,
+  ) {}
+
+  ngOnInit() {
+    this.loadData();
+  }
+
+  loadData() {
+    this.electriciansService.getAllElectricians().subscribe({
+      next: (data) => {
+        this.electricians = data;
+        this.availableElectricians = data.filter((e) => e.isAvailable);
+      },
+    });
+
+    this.requestsService.getAllRequests().subscribe({
+      next: (data) => {
+        this.openRequests = data.filter((r) => r.status === 'Open');
+      },
+    });
+  }
+}
